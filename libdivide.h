2bb079da138f36801b5a1521d2a5f16888814ad4
Alexey Milovidov <milovidov@yandex-team.ru>
2020-07-08 06:50:28 +0300

    Change exit to abort

--- libdivide.h
+++ libdivide.h
@@ -76,7 +76,7 @@
     do { \
         fprintf(stderr, "libdivide.h:%d: %s(): Error: %s\n", \
             __LINE__, LIBDIVIDE_FUNCTION, msg); \
-        exit(-1); \
+        abort(); \
     } while (0)
 
 #if defined(LIBDIVIDE_ASSERTIONS_ON)
@@ -85,7 +85,7 @@
             if (!(x)) { \
                 fprintf(stderr, "libdivide.h:%d: %s(): Assertion failed: %s\n", \
                     __LINE__, LIBDIVIDE_FUNCTION, #x); \
-                exit(-1); \
+                abort(); \
             } \
         } while (0)
 #else
@@ -2037,7 +2037,7 @@ public:
 
 private:
     // Storage for the actual divisor
-    dispatcher<std::is_integral<T>::value, 
+    dispatcher<std::is_integral<T>::value,
                std::is_signed<T>::value, sizeof(T), ALGO> div;
 };
 
