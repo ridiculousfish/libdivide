afb8a8a722c57e10f0dabc028cc24d44fae2a2f1
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-10-26 23:56:09 -0500

    Simplify & clean up the AVR constant div test code (#85)
    * Avoid computing range during each test
    (less test overhead)
    
    * Separate signed & unsigned test environments
    
    * Change checksum type
    
    unsigned long -> int32_t
    
    * Dynamic output based on data type sign
    
    * Simplify constant div tests

--- test/avr/src/invariant_div_test/gen_constant_tests.py
+++ test/avr/src/invariant_div_test/gen_constant_tests.py
@@ -3,6 +3,8 @@ import random
 
 Import("env")
 
+is_unsigned = any((flag for flag in env['BUILD_FLAGS'] if 'TEST_UNSIGNED' in flag))
+
 def eratosthenes():
 	'''Yields the sequence of prime numbers via the Sieve of Eratosthenes.'''
 	D = {}  # map composite integers to primes witnessing their compositeness
@@ -20,7 +22,7 @@ def eratosthenes():
 def before_build():
     def get_denoms(count):
         # 14 signed, 15 unsigned
-        range_end_bits = 14
+        range_end_bits = 15 if is_unsigned else 14
         # Small numbers
         denoms = set([i for i in range(2, 32)])
         # Powers of 2
@@ -31,11 +33,11 @@ def before_build():
         denoms.add((2**range_end_bits)-2)
         # Fill in the rest of the denominators with random
         while (len(denoms)<count):
-            denoms.add(random.randint(33, 2**range_end_bits))
+            denoms.add(random.randint(33, (2**range_end_bits)-1))
         return sorted(denoms)
 
     # Build the denominators
-    denoms = get_denoms(290) # The number of denominators was chosen so that all tests fit on an AtMega2560
+    denoms = get_denoms(340 if is_unsigned else 256) # The number of denominators was chosen so that all tests fit on an AtMega2560
 
     genfile = os.path.join(env['PROJECT_SRC_DIR'], 'invariant_div_test', 'test_declares.g.hpp')
     print(f'Generating {genfile}')
