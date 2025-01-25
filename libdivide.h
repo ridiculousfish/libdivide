66190e9daa603cabe95d99f09fb79b5c186d0417
adbancroft <13982343+adbancroft@users.noreply.github.com>
2025-01-25 20:44:54 +0000

    Auto increment version to 5.2.0

--- libdivide.h
+++ libdivide.h
@@ -12,10 +12,10 @@
 #define LIBDIVIDE_H
 
 // *** Version numbers are auto generated - do not edit ***
-#define LIBDIVIDE_VERSION "5.1.1"
+#define LIBDIVIDE_VERSION "5.2.0"
 #define LIBDIVIDE_VERSION_MAJOR 5
-#define LIBDIVIDE_VERSION_MINOR 1
-#define LIBDIVIDE_VERSION_PATCH 1
+#define LIBDIVIDE_VERSION_MINOR 2
+#define LIBDIVIDE_VERSION_PATCH 0
 
 #include <stdint.h>
 
