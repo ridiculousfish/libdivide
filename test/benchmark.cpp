5dc2d36676e7de41a44c6e86c921eb40fe7af3ee
apocelipes <seve3r@outlook.com>
2025-05-18 01:53:11 +0800

    Fix GCC 15 compiler errors (#147)

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -16,7 +16,7 @@
 
 #include <string.h>
 
-extern "C" int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     // Disable printf buffering.
     // This is mainly required for Windows.
     setbuf(stdout, NULL);
