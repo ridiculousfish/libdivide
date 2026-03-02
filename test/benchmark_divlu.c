d0a6a4d134c346ea22ede0e402122035bfceb6a2
ridiculousfish <corydoras@ridiculousfish.com>
2026-03-02 00:42:07 +0000

    Make divlu benchmark check that results agree with system

--- test/benchmark_divlu.c
+++ test/benchmark_divlu.c
@@ -5,6 +5,7 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 uint32_t divlu(uint32_t numhi, uint32_t numlo, uint32_t den, uint32_t *r);
@@ -51,13 +52,13 @@ static void fill64(uint64_t den) {
     }
 }
 
-// Hardware reference for divlu: a single 64-bit divide.
+// Hardware reference for divlu: a single 64-bit divide and modulo.
 static uint64_t run_hw64(uint32_t den) {
     uint64_t sum = 0;
     uint32_t i;
     for (i = 0; i < ARRAY_LEN; i++) {
         uint64_t num = ((uint64_t)numhi32[i] << 32) | numlo32[i];
-        sum += num / den;
+        sum += num / den + num % den;
     }
     return sum;
 }
@@ -66,7 +67,9 @@ static uint64_t run_divlu(uint32_t den) {
     uint64_t sum = 0;
     uint32_t i;
     for (i = 0; i < ARRAY_LEN; i++) {
-        sum += divlu(numhi32[i], numlo32[i], den, NULL);
+        uint32_t rem;
+        sum += divlu(numhi32[i], numlo32[i], den, &rem);
+        sum += rem;
     }
     return sum;
 }
@@ -75,7 +78,9 @@ static uint64_t run_divllu(uint64_t den) {
     uint64_t sum = 0;
     uint32_t i;
     for (i = 0; i < ARRAY_LEN; i++) {
-        sum += divllu(numhi64[i], numlo64[i], den, NULL);
+        uint64_t rem;
+        sum += divllu(numhi64[i], numlo64[i], den, &rem);
+        sum += rem;
     }
     return sum;
 }
@@ -89,7 +94,7 @@ static uint64_t run_hw128(uint64_t den) {
     uint32_t i;
     for (i = 0; i < ARRAY_LEN; i++) {
         u128 num = ((u128)numhi64[i] << 64) | numlo64[i];
-        sum += (uint64_t)(num / den);
+        sum += (uint64_t)(num / den) + (uint64_t)(num % den);
     }
     return sum;
 }
@@ -98,17 +103,18 @@ static uint64_t run_hw128(uint64_t den) {
 // Prevent the compiler from discarding results.
 static volatile uint64_t sink;
 
-// Time FUNC NTRIALS times, store minimum ns/call in RESULT.
-#define TIME_FUNC(result, func) do {                                     \
+// Time FUNC NTRIALS times, store minimum ns/call in RESULT and last return value in RETVAL.
+#define TIME_FUNC(result, retval, func) do {                             \
     uint64_t _min_ns = UINT64_MAX;                                       \
     int _t;                                                              \
     for (_t = 0; _t < NTRIALS; _t++) {                                  \
         uint64_t _t0 = now_ns();                                         \
-        sink = func;                                                     \
+        (retval) = func;                                                 \
         uint64_t _t1 = now_ns();                                         \
         uint64_t _elapsed = _t1 - _t0;                                  \
         if (_elapsed < _min_ns) _min_ns = _elapsed;                     \
     }                                                                    \
+    sink = (retval);                                                     \
     (result) = _min_ns / (double)ARRAY_LEN;                             \
 } while (0)
 
@@ -117,15 +123,26 @@ int main(void) {
     printf("%10s %10s %10s %10s %10s\n", "den", "divlu", "hw(64b)", "divllu", "hw(128b)");
     while (1) {
         double t_divlu, t_hw64, t_divllu, t_hw128;
+        uint64_t r_divlu, r_hw64, r_divllu, r_hw128;
 
         fill32(den);
-        TIME_FUNC(t_divlu, run_divlu(den));
-        TIME_FUNC(t_hw64,  run_hw64(den));
+        TIME_FUNC(t_divlu, r_divlu, run_divlu(den));
+        TIME_FUNC(t_hw64,  r_hw64,  run_hw64(den));
+        if (r_divlu != r_hw64) {
+            fprintf(stderr, "divlu mismatch for den=%u: divlu=%llu hw64=%llu\n",
+                    den, (ullong)r_divlu, (ullong)r_hw64);
+            abort();
+        }
 
         fill64(den);
-        TIME_FUNC(t_divllu, run_divllu(den));
+        TIME_FUNC(t_divllu, r_divllu, run_divllu(den));
 #if defined(__SIZEOF_INT128__) && !(defined(__clang__) && defined(_MSC_VER))
-        TIME_FUNC(t_hw128, run_hw128(den));
+        TIME_FUNC(t_hw128, r_hw128, run_hw128(den));
+        if (r_divllu != r_hw128) {
+            fprintf(stderr, "divllu mismatch for den=%llu: divllu=%llu hw128=%llu\n",
+                    (ullong)den, (ullong)r_divllu, (ullong)r_hw128);
+            abort();
+        }
 #else
         t_hw128 = 0;
 #endif
